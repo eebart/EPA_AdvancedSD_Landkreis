90b4942bd2cb98cacd1b26e5089e4a47fdfb99fd
JasperDaleman <jasperdaleman@MacBook-Pro-van-Jasper.local>
2017-02-28 14:10:59 +0100

    Analyzed the Shape_file for the included kreise

--- plotting_geopandas.py
+++ plotting_geopandas.py
@@ -5,8 +5,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 GeoFrame = gpd.read_file('vg2500_krs.shp')
-
+'''
 GeoFrame.plot()
 plt.show()
+'''
+# Filter specific Kreise
+#print(GeoFrame.loc[GeoFrame['GEN'] == 'Greiz'])
 
-print(GeoFrame['GEN'])
+# All kreise
+#for kreis in GeoFrame['GEN']:
+#    print(kreis)

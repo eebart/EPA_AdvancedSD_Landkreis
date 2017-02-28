081ba742652d2cc7695c63f766f7105305ff11fc
JasperDaleman <jasperdaleman@MacBook-Pro-van-Jasper.local>
2017-02-28 12:26:24 +0100

    Also needed for plotting

--- plotting_geopandas.py
+++ plotting_geopandas.py
@@ -4,9 +4,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-os.chdir('..')
-os.chdir('Data/Geographic/vg2500_geo84')
-
 GeoFrame = gpd.read_file('vg2500_krs.shp')
 
 GeoFrame.plot()

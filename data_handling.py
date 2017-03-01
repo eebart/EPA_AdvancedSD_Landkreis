cd4786cc05100b993b6f41c95e2795e483609161
eebart <eebartholomew@gmail.com>
2017-03-01 20:09:43 +0100

    Update CSVs to english, starting to process columns for landkreise names

--- data_handling.py
+++ data_handling.py
@@ -5,44 +5,41 @@ import csv
 
 os.chdir('AllDataFromDatabase')
 
-births = pd.read_csv('Births.csv', skiprows=4, engine='python',
-                     skipfooter=4, header=1, index_col=0, sep=';',
-                     encoding='latin1')
-basicInfoHospitals = pd.read_csv('BasicInfoHospitals.csv', skiprows=4,
-                                 engine='python', skipfooter=4, header=1,
-                                 index_col=0, sep=';', encoding='latin1')
-RehabAndPrevention = pd.read_csv('BasicInfoHospitalsRehabilitationAndPrevention.csv',
-                                 skiprows=4, engine='python', skipfooter=4,
-                                 header=1, index_col=0, sep=';',
-                                 encoding='latin1')
-deaths = pd.read_csv('Deaths.csv', skiprows=4, engine='python',
-                     skipfooter=4, header=1, index_col=0, sep=';',
-                     encoding='latin1')
-higherEdu = pd.read_csv('HigherEducated.csv', skiprows=4, engine='python',
-                     skipfooter=4, header=1, index_col=0, sep=';',
-                     encoding='latin1')
-AgeGender = pd.read_csv('PopulationAgeGender.csv', skiprows=4, engine='python',
-                         skipfooter=4, header=3, index_col=0, sep=';',
-                         encoding='latin1')
+landkreise = pd.read_csv('Landkreise.csv',encoding='UTF-8')
+
+births = pd.read_csv('Births.csv',encoding='UTF-8')
+basicInfoHospitals = pd.read_csv('BasicInfoHospitals.csv',
+                                 engine='python',encoding='UTF-8')
+rehabAndPrevention = pd.read_csv('BasicInfoHospitalsRehabilitationAndPrevention.csv',
+                                 engine='python',encoding='UTF-8')
+deaths = pd.read_csv('Deaths.csv',
+                     engine='python',encoding='UTF-8')
+higherEdu = pd.read_csv('HigherEducated.csv',
+                         engine='python',header=2,encoding='UTF-8')
+ageGender = pd.read_csv('PopulationAgeGender.csv',
+                         engine='python',header=1,encoding='UTF-8')
 workingAge = pd.read_csv('SomethingLikePeopleWorkinh.csv',
-                          skiprows=4, engine='python', skipfooter=4,
-                          header=2, index_col=0, sep=';',
-                          encoding='latin1')
-surface = pd.read_csv('SurfaceSquareKM.csv', skiprows=4, engine='python',
-                     skipfooter=4, header=1, index_col=0, sep=';',
-                     encoding='latin1')
-unemployed = pd.read_csv('unemployed.csv', skiprows=4, engine='python',
-                     skipfooter=4, header=1, index_col=0, sep=';',
-                     encoding='latin1')
-WorthArea = pd.read_csv('WorthArea.csv', skiprows=4, engine='python',
-                     skipfooter=4, header=1, index_col=0, sep=';',
-                     encoding='latin1')
-
-#print(births.head(10))
-
+                         engine='python',header=1,encoding='UTF-8')
+surface = pd.read_csv('SurfaceSquareKM.csv',
+                      engine='python',encoding='UTF-8')
+unemployed = pd.read_csv('unemployed.csv',
+                         engine='python',encoding='UTF-8')
+WorthArea = pd.read_csv('WorthArea.csv',
+                         engine='python',encoding='UTF-8')
 
 headers = list(births)
-stadte = births[headers[1]]
+# TODO Match names in landkreise with names in each file (string parsing, wheee!!!)
+birth_kreise = births[births[headers[1]].str.contains('kreis|Kreis')]
+matches = {}
+for index, row in landkreise.iterrows():
+    match = None
+    for district in row['District'].split(';'):
+        match = birth_kreise[birth_kreise['Name'].str.contains(district,case=False)]
+        # TODO replace Name with District string
+    matches[district] = match
+    if (len(match)>1):
+        print(row.District)
+        print(match)
+        # TODO this should be empty. Solve these errors
 
-kreise = births[births[headers[1]].str.contains('kreis')]
-kreise = kreise.append(births[births[headers[1]].str.contains('Kreis')])
+# kreise.to_csv('test.csv',encoding='UTF-8')

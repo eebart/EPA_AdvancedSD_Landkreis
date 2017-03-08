1d35b8290477204d1f274499ade0d96fe49653ff
eebart <eebartholomew@gmail.com>
2017-03-08 10:38:03 +0100

    clean up data script

--- data_handling.py
+++ data_handling.py
@@ -1,5 +1,5 @@
-import numpy as np
-import pandas as pd
+import numpy as numpy
+import pandas as pandas
 
 def fixWorthArea(worthArea):
     groups = worthArea.groupby(['Index','Name'])
@@ -22,50 +22,50 @@ def fixWorthArea(worthArea):
 
         allGroups.append(dataRow)
 
-    updated = pd.DataFrame(allGroups,columns=columns)
-    return worthArea
+    updated = pandas.DataFrame(allGroups,columns=columns)
+    return updated
 
 def loadCSVs(parentdir):
     return {
-        'births':pd.read_csv(parentdir + 'Births.csv',encoding='UTF-8'),
-        'hospital':pd.read_csv(parentdir + 'BasicInfoHospitals.csv',encoding='UTF-8'),
-        'deaths':pd.read_csv(parentdir + 'Deaths.csv',encoding='UTF-8'),
-        'edu':pd.read_csv(parentdir + 'HigherEducated.csv',encoding='UTF-8'),
-        'age':pd.read_csv(parentdir + 'PopulationAgeGender.csv',encoding='UTF-8'),
-        'workingAge':pd.read_csv(parentdir + 'SomethingLikePeopleWorkinh.csv',encoding='UTF-8'),
-        'surface':pd.read_csv(parentdir + 'SurfaceSquareKM.csv',encoding='UTF-8'),
-        'unemployed':pd.read_csv(parentdir + 'unemployed.csv',encoding='UTF-8'),
-        'worthArea':fixWorthArea(pd.read_csv(parentdir + 'WorthArea.csv',encoding='UTF-8'))
+        'births':pandas.read_csv(parentdir + 'Births.csv',encoding='UTF-8'),
+        'hospital':pandas.read_csv(parentdir + 'BasicInfoHospitals.csv',encoding='UTF-8'),
+        'deaths':pandas.read_csv(parentdir + 'Deaths.csv',encoding='UTF-8'),
+        'edu':pandas.read_csv(parentdir + 'HigherEducated.csv',encoding='UTF-8'),
+        'age':pandas.read_csv(parentdir + 'PopulationAgeGender.csv',encoding='UTF-8'),
+        'workingAge':pandas.read_csv(parentdir + 'SomethingLikePeopleWorkinh.csv',encoding='UTF-8'),
+        'surface':pandas.read_csv(parentdir + 'SurfaceSquareKM.csv',encoding='UTF-8'),
+        'unemployed':pandas.read_csv(parentdir + 'unemployed.csv',encoding='UTF-8'),
+        'worthArea':fixWorthArea(pandas.read_csv(parentdir + 'WorthArea.csv',encoding='UTF-8'))
     }
 
 #Run initially to verify that landkreise names are common for all elements
 def checkNameCommonality(allFrames):
     for index in allFrames:
-        if (not allFrames[index]['Index'].dtype == np.int64):
+        if (not allFrames[index]['Index'].dtype == numpy.int64):
             allFrames[index]['Name']=allFrames[index]['Name'].str.strip()
             allFrames[index] = allFrames[index][allFrames[index]['Index'].astype(str).str.isdigit()].copy()
-            allFrames[index]['Index'] = pd.to_numeric(allFrames[index]['Index'])
+            allFrames[index]['Index'] = pandas.to_numeric(allFrames[index]['Index'])
 
     frames = list(allFrames.values())
 
     mismatched = False
     for i in range(len(frames)):
         for j in range(i+1,len(frames)):
-            test = pd.merge(frames[i],frames[j], on=['Index'], how='outer')
+            test = frames[i].merge(frames[j], on=['Index'], how='outer').dropna()
             test['matched'] = test['Name_x'] == test['Name_y']
-            test = test[['Index','Name_x','Name_y','matched']].dropna()
+            test = test[['Index','Name_x','Name_y','matched']]
             if (len(test[test['matched']==False])>0):
                 print('Index {} and {} did not match: {}'.format(i,j,len(test[test['matched']==False])))
                 print(test[test['matched']==False].to_string(index=False))
                 mismatched = True
 
     if (not mismatched):
-        print('All common names matched up. Good to go!')
+        print('All names matched up. Good to go!')
 
 def selectLandkreise(dataDir):
     # Make sure that we only have the 402 Landkreise left in the superduper dataframe.
     everything = loadEverything()
-    landkreise = pd.read_csv(dataDir + 'Landkreise.csv',encoding='UTF-8')
+    landkreise = pandas.read_csv(dataDir + 'Landkreise.csv',encoding='UTF-8')
 
     landkreise['Type'] = landkreise['Type'].replace('', 'Kreis')
     cleaned = landkreise.merge(everything.drop('Name',1),on=['Index'],how='inner')
@@ -77,35 +77,35 @@ def selectLandkreise(dataDir):
 
 def saveEverything(allFrames):
     for index in allFrames:
-        if (not allFrames[index]['Index'].dtype == np.int64):
+        if (not allFrames[index]['Index'].dtype == numpy.int64):
             allFrames[index]['Name']=allFrames[index]['Name'].str.strip()
             allFrames[index] = allFrames[index][allFrames[index]['Index'].astype(str).str.isdigit()].copy()
-            allFrames[index]['Index'] = pd.to_numeric(allFrames[index]['Index'])
+            allFrames[index]['Index'] = pandas.to_numeric(allFrames[index]['Index'])
 
     # drop name column from everything except first, then merge all into a large group
-    everything = pd.DataFrame()
+    everything = pandas.DataFrame()
     for index in allFrames:
+        frame = allFrames[index].copy()
         cols = []
-        for col in allFrames[index]:
+        for col in frame:
             if col == 'Name' or col == 'Index':
                 cols.append(col)
             else:
                 cols.append(col+'_'+index)
-        allFrames[index].columns = cols
+        frame.columns = cols
 
         if len(everything) == 0:
-            everything = allFrames[index]
+            everything = frame
         else:
-            allFrames[index] = allFrames[index].drop('Name',1)
-            everything = everything.merge(allFrames[index],on=['Index'],how='outer')
+            frame = frame.drop('Name',1)
+            everything = everything.merge(frame,on=['Index'],how='outer')
 
-    print('Saving everything to super duper data file')
-
-    everything.to_csv('everything.csv',encoding='UTF-8',index=False)
+    everything.to_csv(dataDir + 'everything.csv',encoding='UTF-8',index=False)
+    print('Done creating superduper data file.')
     return everything
 
 def loadEverything():
-    return pd.read_csv('everything.csv',encoding='UTF-8')
+    return pandas.read_csv(dataDir + 'everything.csv',encoding='UTF-8')
 
 dataDir = 'AllDataFromDatabase/'
 

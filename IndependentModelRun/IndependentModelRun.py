36593b9732cd02d0552fe165fb8331ef914c0190
eebart <eebartholomew@gmail.com>
2017-04-07 13:54:22 +0200

    IndependentModelRun checkin

---
+++ IndependentModelRun/IndependentModelRun.py
@@ -0,0 +1,192 @@
+import tarfile
+import os
+import numpy as np
+import pandas as pd
+
+from vensimConnector import *
+
+class Result():
+    def __init__(self, outcome_var, outcomes):
+        self.variable = outcome_var
+        self.outcomes = outcomes
+
+class Case():
+    def __init__(self, ident, name='', variables={}):
+        self.id = ident
+        self.name = name if name != '' else ident
+        self.vars = variables
+        self.result_file = ''
+        self.results = []
+
+class Model():
+
+    def __init__(self, model_name, wd, model_file, cases=[]):
+        if not model_file.endswith('.vpm'):
+            raise ValueError('model file should be a vpm file')
+
+        path_to_file = os.path.join(wd, model_file)
+        if not os.path.isfile(path_to_file):
+            raise ValueError('cannot find model file')
+
+        self.name = model_name
+        self.working_directory = wd
+        self.model_file = model_file
+
+        self.results_dir = 'results/'
+        try:
+            os.stat(self.results_dir)
+        except:
+            os.mkdir(self.results_dir)
+
+        self.outcomes = ['TIME']
+        self.cases = cases
+
+    def build_cases_from_dict(self,case_dict):
+        curr_index = 0
+        name, ident = '', ''
+
+        self.cases = []
+        for record in case_dict:
+            if 'id' in record:
+                ident = record['id']
+                del record['id']
+            else:
+                ident = str(curr_index)
+
+            if 'name' in record:
+                name = record['name']
+                del record['name']
+
+            self.cases.append(Case(ident,name,record))
+            curr_index += 1
+
+    def add_outcomes(self, outcome_list):
+        self.outcomes.extend(outcome_list)
+
+    def __model_init(self):
+        fn = os.path.join(self.working_directory, self.model_file)
+        load_model(fn) #EMA
+        be_quiet() #EMA
+
+        try:
+            initialTime  = get_value('INITIAL TIME')
+            finalTime = get_value('FINAL TIME')
+            timeStep = get_value('TIME STEP')
+            savePer = get_value('SAVEPER')
+
+            if savePer > 0:
+                timeStep = savePer
+
+            self.run_length = int((finalTime - initialTime)/timeStep +1)
+
+        except Exception:
+            raise Exception
+
+    def __parse_lookup(self, lookup_file, case):
+        df = pd.read_csv(lookup_file,encoding='UTF-8')
+
+        vals = {}
+        if 'id' in df.columns:
+            ids = df.loc[df['id'] == case.id]
+            if ids.shape[0] > 0:
+                vals = ids.iloc[0].to_dict()
+        if 'name' in df.columns:
+            names = df.loc[df['name'] == case.name]
+            if names.shape[0] == 1:
+                vals = names.iloc[0].to_dict()
+
+
+        to_set = []
+        for key, val in vals.items():
+            if key != 'name' and key != 'id':
+                to_set.append((float(key),val))
+
+        return to_set
+
+    def __reset_model(self):
+        self._output = {}
+
+    def run_cases(self):
+        self.__model_init()
+
+        results_files = {}
+
+        for index, case in enumerate(self.cases):
+
+            for key, value in case.vars.items():
+                var_type = get_varattrib(key, 14) #get the type of the variable
+                if var_type[0].lower() == 'lookup':
+                    #it's a lookup, value is actually filename for lookup
+                    lookup_value = self.__parse_lookup(value, case)
+                    set_value(key, lookup_value)
+                else:
+                    set_value(key, value)
+
+            try:
+                print("Running simulation for '{}'".format(case.name))
+                case.result_file = self.results_dir + case.id + '_' + case.name + '.vdf'
+                run_simulation(case.result_file)
+            except Exception:
+                raise
+
+        self.get_results()
+
+    def __check_data(self, result):
+        error = False
+        if len(result[0]) != self.run_length:
+            data = np.empty((self.run_length))
+            data[:] = np.NAN
+            data[0:len(result[0])] = result
+            result = data
+            error = True
+        return result, error
+
+    def get_results(self):
+        error = False
+        badcount = 0
+
+        for case in self.cases:
+            result_filename = os.path.join(self.working_directory, case.result_file)
+            for variable in self.outcomes:
+                res = get_data(result_filename, variable)
+                result, er = self.__check_data(np.asarray(res))
+                error = error or er
+                case.results.append(Result(variable, np.array(result)))
+
+            if error:
+                badcount += 1
+
+        print('{} out of {} cases ran into difficulties.'.format(badcount, len(self.cases)))
+
+def save_results(cases, results_file):
+    keys = set()
+
+    results_frames = {}
+    time_frame = []
+    for case in cases:
+        for result in case.results:
+            if (result.variable == 'TIME'):
+                timeoutcome = result.outcomes[0]
+                timeoutcome = timeoutcome[np.logical_not(np.isnan(timeoutcome))].tolist()
+                if len(time_frame) < len(timeoutcome):
+                    time_frame = timeoutcome
+            else:
+                outcome_arr = [case.id,case.name]
+                outcome_arr.extend(result.outcomes[0])
+                if result.variable not in results_frames:
+                    results_frames[result.variable] = [outcome_arr]
+                else:
+                    results_frames[result.variable].append(outcome_arr)
+
+    tar = tarfile.open(results_file, 'w:gz')
+
+    columns = ['id','name']
+    columns.extend(time_frame)
+    for key in results_frames:
+        df = pd.DataFrame(results_frames[key],columns=columns)
+        filename = 'outcome_'+key+'.csv'
+        df.to_csv(filename, index=False)
+        tar.add(filename)
+        os.remove(filename)
+
+    tar.close()
---
+++ IndependentModelRun/vensimConnector.py
@@ -0,0 +1,291 @@
+'''
+Adapted from EMA Workbench
+https://github.com/quaquel/EMAworkbench
+
+by default it is assumed the dll is readily available. If this generates an
+VensimError, you have to find the location of the dll and either copy it to
+C:\Windows\System32 and/or C:\Windows\SysWOW64, or use::
+
+    vensim = ctypes.windll.LoadLibrary('location of dll')
+
+Typically, the dll can be found in ../AppData/Local/Vensim/vendll32.dll
+
+Created 2017
+@author: eebart
+'''
+import ctypes
+import numpy as np
+
+__all__ = ['be_quiet',
+           'load_model',
+           'run_simulation',
+           'get_varattrib',
+           'get_value',
+           'set_value',
+           'get_data',
+           'command']
+
+try:
+    WindowsError # @UndefinedVariable
+except NameError:
+    WindowsError = None
+
+def be_quiet(quietflag=2):
+    '''
+    this allows you to turn off the work in progress dialog that Vensim
+    displays during simulation and other activities, and also prevent the
+    appearance of yes or no dialogs.
+
+    use 0 for normal interaction, 1 to prevent the appearance of any work
+    in progress windows, and 2 to also prevent the appearance of any
+    interrogative dialogs'
+    '''
+    if quietflag > 2:
+        raise VensimError("incorrect value for quietflag")
+
+    return vensim.vensim_be_quiet(quietflag)
+
+def load_model(file_name):
+    '''
+    load the model
+
+    Parameters
+    ----------
+    file_name : str file name of model, relative to working directory
+
+    Raises
+    -------
+    Exception if the model cannot be loaded.
+
+    Note: only works for .vpm files
+    '''
+    try:
+        command("SPECIAL>LOADMODEL|"+str(file_name))
+    except Exception as w:
+        raise Exception("vensim file not found")
+
+def run_simulation(file_name):
+    '''
+    Convenient function to run a model and store the results of the run in
+    the specified .vdf file. The specified output file will be overwritten
+    by default
+
+    Parameters
+    ----------
+    file_name : str
+                the file name of the output file relative to the working
+                directory
+
+    Raises
+    ------
+    Exception if running the model failed in some way.
+    '''
+
+    file_name = str(file_name)
+    try:
+        command("SIMULATE>RUNNAME|"+file_name+"|O")
+        command("MENU>RUN|o")
+    except Exception as e:
+        raise Exception(str(e))
+
+def get_varattrib(varname, attribute):
+    '''
+    This function can be used to access the attributes of a variable.
+
+    Parameters
+    ----------
+    varname : str
+              name for which you want attribute
+    attribute : int
+                attribute you want
+
+    Notes
+    -----
+
+    ====== =============
+    number meaning
+    ====== =============
+    1      Units,
+    2      the comment,
+    3      the equation,
+    4      causes,
+    5      uses,
+    6      initial causes only,
+    7      active causes only,
+    8      the subscripts the variable has,
+    9      all combinations those subscripts create,
+    10     the combination of subscripts that would be used by a graph tool,
+    11     the minimum value set in the equation editor,
+    12     the maximum and
+    13     the range,
+    14     the variable type (returned as "Level" etc) and
+    15     the main group of a variable
+    ====== =============
+
+    '''
+    buf = ctypes.create_string_buffer(''.encode('utf-8'), 10)
+    maxBuf = ctypes.c_int(10)
+
+    bufferlength = vensim.vensim_get_varattrib(varname.encode('utf-8'),
+                                               attribute,
+                                               buf,
+                                               maxBuf)
+    if bufferlength == -1:
+        raise VensimWarning("variable not found")
+
+    buf = ctypes.create_string_buffer(''.encode('utf-8'), int(bufferlength))
+    maxBuf = ctypes.c_int(int(bufferlength))
+    vensim.vensim_get_varattrib(varname.encode('utf-8'), attribute, buf, maxBuf)
+
+    result = repr(buf.raw.decode('utf-8'))
+    result = result.strip()
+    result = result.rstrip("'")
+    result = result.lstrip("'")
+    result = result.split(r"\x00")
+    result = [varname for varname in result if len(varname) != 0]
+
+    return result
+
+def get_value(name):
+    '''
+    This function returns the value of a variable during a simulation, as a
+    game is progressing, or during simulation setup
+
+    Parameters
+    ----------
+    name : str
+           the name of variable for which one wants to retrieve the value.
+
+    '''
+    value = ctypes.c_float(0)
+    return_val = vensim.vensim_get_val(name.encode('utf-8'), ctypes.byref(value))
+    if return_val == 0:
+        raise VensimWarning("variable not found")
+
+    return value.value
+
+def set_value(variable, value):
+    '''
+    set the value of a variable to value
+
+    current implementation only works for lookups and normal values. In case
+    of a list, a lookup is assumed, else a normal value is assumed.
+    See the DSS reference supplement, p. 58 for details.
+
+    Parameters
+    ----------
+    variable : str
+               name of the variable to set.
+    value : int, float, or list
+            the value for the variable. **note**: the value can be either a
+            list, or an float/integer. If it is a list, it is assumed the
+            variable is a lookup.
+    '''
+    variable = str(variable)
+
+    if isinstance(value, list):
+        value = [str(entry) for entry in value]
+        command("SIMULATE>SETVAL|"+variable+"("+ str(value)[1:-1] + ")")
+    else:
+        try:
+            command("SIMULATE>SETVAL|"+variable+"="+str(value))
+        except VensimWarning:
+            warning('variable: \'' +variable+'\' not found')
+
+def get_data(filename, variable_name, tname="Time"):
+    '''
+    Retrieves data from simulation runs or imported data sets. In contrast
+    to the Vensim DLL, this method retrieves all the data, and not only the
+    data for the specified length.
+
+    Parameters
+    ----------
+    filename : str
+               the name of the .vdf file that contains the data
+    variable_name : str
+             the name of the variable to retrieve data on
+    tname : str
+            the name of the time axis against which to pull the data,
+            by default this is Time
+
+    Returns
+    -------
+    a tuple with an  for an array for varname and and array for tname.
+
+    '''
+    vval = (ctypes.c_float * 1)()
+    tval = (ctypes.c_float * 1)()
+    maxn = ctypes.c_int(0)
+
+    filename = filename.encode('utf-8')
+    varname = variable_name.encode('utf-8')
+    tname = tname.encode('utf-8')
+
+    return_val = vensim.vensim_get_data(filename,
+                                        varname,
+                                        tname,
+                                        vval,
+                                        tval,
+                                        maxn)
+
+    if return_val == 0:
+        raise VensimWarning("variable "+variable_name+" not found in dataset")
+
+    vval = (ctypes.c_float * int(return_val))()
+    tval = (ctypes.c_float * int(return_val))()
+    maxn = ctypes.c_int(int(return_val))
+
+    return_val = vensim.vensim_get_data(filename,\
+                                         varname,\
+                                         tname,\
+                                         vval,\
+                                         tval,\
+                                         maxn)
+
+    vval = np.ctypeslib.as_array(vval)
+    tval = np.ctypeslib.as_array(tval)
+
+    return vval, tval
+
+def command(command):
+    '''execute a command, for details see chapter 5 of the vensim DSS manual'''
+
+    return_val = vensim.vensim_command(command.encode('utf-8'))
+    if return_val == 0:
+        raise Exception("command failed "+command)
+    return return_val
+
+def add_dll():
+    try:
+        WindowsError # @UndefinedVariable
+    except NameError:
+        WindowsError = None
+
+    try:
+        vensim_single = ctypes.windll.vendll32
+    except AttributeError:
+        vensim_single = None
+    except WindowsError:
+        vensim_single = None
+
+    try:
+        vensim_double = ctypes.windll.LoadLibrary('C:\Windows\SysWOW64\VdpDLL32.dll')
+    except AttributeError:
+        vensim_double = None
+    except WindowsError:
+        vensim_double = None
+
+    global vensim
+    if vensim_single and vensim_double:
+        vensim = vensim_single
+        print("both single and double precision vensim available, using single")
+    elif vensim_single:
+        vensim = vensim_single
+        print('using single precision vensim')
+    elif vensim_double:
+        vensim = vensim_double
+        print('using single precision vensim')
+    else:
+        print("vensim dll not found, vensim functionality not available")
+
+add_dll()
